9682879a8be3b4bec430382f9791fb1df6187215
Xyloflake <xyloflake@gmail.com>
2024-09-18 21:01:32 +0530

    Update patch-package.js

--- patch-package.js
+++ patch-package.js
@@ -28,7 +28,7 @@ packageJson.name = 'mufbeta';
 packageJson.version = version;
 packageJson.repository = 'https://github.com/muffonlabs/mufbeta';
 packageJson.author = {
-    email: "staniel359@gmail.com",
+    email: "muffonlabs@gmail.com",
     name: 'muffonlabs',
     url: 'https://github.com/muffonlabs',
 };
